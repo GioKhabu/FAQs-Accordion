60b55c92e8e9fbaf341324e6a0a550ffa090fa39
unknown <giokhabu@gamil.com>
2023-12-12 17:15:40 +0400

    init: created FAQs accordion full functionality

---
+++ src/assets/utils.js
@@ -0,0 +1,26 @@
+export let data = [
+  {
+    title: 'What is Frontend Mentor, and how will it help me?',
+    description:
+      'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building.',
+    status: true,
+  },
+  {
+    title: 'Is Frontend Mentor free?',
+    description:
+      'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building.',
+    status: false,
+  },
+  {
+    title: 'Can I use Frontend Mentor projects in my portfolio?',
+    description:
+      'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building.',
+    status: false,
+  },
+  {
+    title: "How can I get help if I'm stuck on a challenge?",
+    description:
+      'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building.',
+    status: false,
+  },
+];
