c286b18d7c216be0e750d2051f04121877cbabd9
unknown <giokhabu@gamil.com>
2023-12-15 12:16:34 +0400

    feat: update data and some styles

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -8,19 +8,19 @@ export let data = [
   {
     title: 'Is Frontend Mentor free?',
     description:
-      'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building.',
+      'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.',
     status: false,
   },
   {
     title: 'Can I use Frontend Mentor projects in my portfolio?',
     description:
-      'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building.',
+      "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
     status: false,
   },
   {
     title: "How can I get help if I'm stuck on a challenge?",
     description:
-      'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building.',
+      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
     status: false,
   },
 ];
